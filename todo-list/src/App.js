785aeeff60d4af0b8d2d7d35ff519b96e4bdfca2
Vi <vetttaa@mail.ru>
2023-12-25 15:03:26 +0300

    gitignor

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Hi</h1>
+    <div className="container">
+    <Header title='Hello'/>
     </div>
   );
 }
